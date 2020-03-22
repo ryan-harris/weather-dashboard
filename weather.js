f85d723399b9affd9367d3c2d15e8e9dc84968ec
Ryan Harris <harris.ucla@gmail.com>
2020-03-22 00:45:28 -0700

    Implemented current weather and UV-index API calls

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 const HISTORY_KEY = "weatherCitySearchHistory";
 const HISTORY_BTN_BS_CLASSES = "btn btn-light border text-left px-3 py-2";
 const HISTORY_DATA_CITY_ATTR = "data-city";
+const API_KEY = "d77358b957f48bb5e0d0bc47f76aad4e";
 
 let history = JSON.parse(localStorage.getItem(HISTORY_KEY));
 if (!history) {
@@ -25,7 +26,41 @@ function init() {
 }
 
 function renderCityWeather(city) {
-  // console.log(`Render city ${city}`);
+  // hide weather divs on html page
+  $("#current-weather").attr("style", "display: none;")
+  $("#current-uv-element").attr("style", "display: none;")
+  $("#future-weather").attr("style", "display: none;")
+
+  let currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + API_KEY + "&q=" + city;
+  $.ajax({
+    url: currentQueryURL,
+    method: "GET"
+  }).then(function(response) {
+    // start the UV index ajax call first (need lat and lon from weather call)
+    let uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + API_KEY + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+    $.ajax({
+      url: uvQueryURL,
+      method: "GET"
+    }).then(function(response) {
+      let uvIndex = response.value;
+      $("#current-uv").text(uvIndex)
+      // set background color and text color based on uv index
+      $("#current-uv").attr("style", getUVColorStyle(uvIndex))
+
+      // show uv element
+      $("#current-uv-element").attr("style", "display: block;")
+    });
+
+    // update current weather on page
+    $("#current-city").text(response.name + " (" + moment().format("l") + ")")
+    $("#current-icon").attr("src", getWeatherIconURL(response.weather[0].icon))
+    $("#current-temp").text(response.main.temp.toFixed(1));
+    $("#current-humidity").text(response.main.humidity);
+    $("#current-wind").text(response.wind.speed);
+
+    // show current weather div
+    $("#current-weather").attr("style", "display: block;");
+  });
 }
 
 function handleSearch(event) {
@@ -42,7 +77,6 @@ function handleSearch(event) {
 }
 
 function handleHistoryItemClick(event) {
-  console.log(event.target);
   if (event.target.matches("button")) {
     renderCityWeather($(event.target).attr(HISTORY_DATA_CITY_ATTR));
   }
@@ -75,6 +109,20 @@ function addHistoryItem(city) {
   // }
 }
 
-function convertKelvinToFarenheit(kelvin) {
-  return ((kelvin - 273.15) * 9/5 + 32).toFixed(1);
+function getUVColorStyle(uvIndex) {
+  if (uvIndex <= 2) {
+    return "background-color: green; color: white;";
+  } else if (uvIndex <= 5) {
+    return "background-color: gold; color: black";
+  } else if (uvIndex <= 7) {
+    return "background-color: goldenrod; color: black;";
+  } else if (uvIndex <= 10) {
+    return "background-color: firebrick; color: white;";
+  } else {
+    return "background-color: violet; color: black;";
+  }
+}
+
+function getWeatherIconURL(iconCode) {
+  return "http://openweathermap.org/img/w/" + iconCode + ".png";
 }
